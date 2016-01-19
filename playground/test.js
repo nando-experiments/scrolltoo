9cb136d207c4f1d216490aed509343bd9277346c
Fernando Moreira <nandomoreira.me@gmail.com>
2016-01-19 13:41:13 -0200

    add scrolltoo top

--- playground/test.js
+++ playground/test.js
@@ -1,10 +1,27 @@
 (function($) {
   'use strict';
 
+  var $navbar = $('.navbar a'),
+      $scrollTooTop = $('.scrolltoo-top');
+
   $(document).ready(function() {
-    $('.navbar a').scrolltoo({
-      speed: 1200
+
+    $navbar.scrolltoo();
+
+    $scrollTooTop.scrolltoo({
+      speed: 300
     });
+
+  });
+
+  $(document).scroll(function() {
+    var $one = $('#one').height();
+
+    if ($(this).scrollTop() > $one) {
+      $scrollTooTop.css({ 'bottom' : '15px' });
+    } else {
+      $scrollTooTop.css({ 'bottom' : '-80px' });
+    }
   });
 
 })(jQuery);
